f4cf001e699d0a1be7a6ab15a99fbdf7bdc54403
Tomas Matteson <tomasmatteson@unknownc2a511a25b90.attlocal.net>
2024-01-02 17:56:46 -0600

    check consistency

--- multi-v2.py
+++ multi-v2.py
@@ -123,13 +123,6 @@ def get_PLLR(model, alphabet, data_loader, batches, device_id, args):
                 seq = strs[j]
                 idx=[alphabet.tok_to_idx[i] for i in seq]
                 PLLR = np.sum(np.diag(s_j[:,idx]))
-                if j == 1:
-                    print(f"labels: {labels[j]}")
-                    print(f"toks: {toks[j]}")
-                    print(f"idx: {idx}")
-                    print(f"seq: {seq}")
-                    print(f"PLLR: {PLLR}")
-                    raise Error
 
                 PLLRs[j] = PLLR
                 args.output_file = args.output_dir / f"{labels[j]}.pt"
