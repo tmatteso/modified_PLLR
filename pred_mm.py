11bd3768cd7ebc73730200b50bcf987abe1e3156
Tomas Matteson <tomasmatteson@Tomass-MBP-2.ucsfmedicalcenter.org>
2024-01-05 13:51:47 -0800

    check consistency

--- pred_mm.py
+++ pred_mm.py
@@ -520,14 +520,14 @@ def plot_all_results(results_path):
     grouped = all_assays.groupby(['assay', 'dist_from_WT', 'eval_size'])
     for (assay, dist_from_WT, eval_size), group_data in grouped:
         results_bargraph(group_data,
-                         f'Distance from WT: {dist_from_WT}',
+                         f'Assay: {assay}, Distance from WT: {dist_from_WT}, Evaluation Size:{eval_size}', 
                          f"SM_pred_{assay}_{dist_from_WT}.png")
         
     # now we make one for each distance from wildtype
     grouped = all_assays.groupby(['dist_from_WT'])
     for (dist_from_WT), group_data in grouped:
         results_bargraph(group_data,
-                    f'Assay: {assay}, Distance from WT: {dist_from_WT}, Evaluation Size:{eval_size}',
+                    f'Distance from WT: {dist_from_WT}',
                     f"SM_pred_{dist_from_WT}_all_assays.png")
 
 def main(args):
