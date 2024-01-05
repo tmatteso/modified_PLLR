bab5c11595f6beb7f16cbdc12b77f84b9d7eb57b
Tomas Matteson <tomasmatteson@Tomass-MBP-2.ucsfmedicalcenter.org>
2024-01-05 15:04:40 -0800

    check consistency

--- pred_mm.py
+++ pred_mm.py
@@ -534,7 +534,7 @@ def plot_all_results(results_path):
     
     
     # Remove rows with duplicate assay names
-    all_assays = all_assays.drop_duplicates(subset=['assay_tokens', 'eval_size'], keep='first')    
+    all_assays = all_assays.drop_duplicates(subset=[ 'eval_size'], keep='first')  #subset=['assay_tokens', 'eval_size'], keep='first')    
     print(len(all_assays.assay.unique()))
     raise Error
     #print(all_assays[all_assays.assay == "RBP1_HUMAN_Rocklin_2023_2KWH.csv"])
