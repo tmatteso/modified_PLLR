7bdf91835fa555d78db388963eb7a8b0fb40150a
Tomas Matteson <tomasmatteson@Tomass-MBP-2.ucsfmedicalcenter.org>
2024-01-09 18:25:29 -0800

    check consistency

--- pred_mm.py
+++ pred_mm.py
@@ -533,13 +533,13 @@ def results_lineplot(group_data, title, figname):
         # one hot without embeddings
         "sum_DMS": "blue",
         "one_hot": "darkblue",
-        "one_hot+sum_LLR": "lightblue",
+        "one_hot+sum_LLR": "cyan",
         'PLLR+sum_LLR': "magenta",
         # ESM layer or ESM layer + sum LLR
         "layer_21": "green",
         "layer_33": "yellow",
         "layer_21+sum_LLR": "lightgreen",
-        "layer_33+sum_LLR": "orange",
+        "layer_33+sum_LLR": "darkorange",
         "21+33+LLR": "purple",
         # wambo combos
         "oh+21+33+LLR": "brown",
