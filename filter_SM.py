ab604dbcb691787060eefe029148edb008c3f7e9
tmatteso <tomasmatteson@Tomass-MBP-2.ucsf.edu>
2024-02-01 11:49:31 -0800

    filter SM

--- filter_SM.py
+++ filter_SM.py
@@ -195,7 +195,10 @@ def eval_loop(intersect_set, desired, full, LLRS, output_csv):
             if mm.index.size != 0:
                 # break up each mutation in mm into its constituent parts
                 sm_in_mm = mm.mutant.str.split(":", expand=True)
-                print(sm_in_mm)
+                sm_ls = []
+                for column in sm_in_mm.columns:
+                    sm_ls += sm_in_mm[column].unique()
+                print(sm_ls)
                 raise error
             # then 
             #print(sm)
