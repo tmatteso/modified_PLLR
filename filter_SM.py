806bd7e736dce882d55d70f21bb50bb1c154a115
tmatteso <tomasmatteson@Tomass-MBP-2.ucsfmedicalcenter.org>
2024-02-02 12:51:48 -0800

    filter SM

--- filter_SM.py
+++ filter_SM.py
@@ -243,8 +243,9 @@ def main(args):
     if not args.graphs_only:
         # desired assays:
         if args.only_assay is None:
-            query_string =  "../ESM_variant_sweep/Protein_Gym/ProteinGym_substitutions/SDA_BACSU_Rocklin_2023_1PV0.csv"
-            #f"{args.pg_sub_dir}/*.csv"
+            query_string =  f"{args.pg_sub_dir}/*.csv"
+            #"../ESM_variant_sweep/Protein_Gym/ProteinGym_substitutions/SDA_BACSU_Rocklin_2023_1PV0.csv"
+            #
             intersect_set, full = read_in_PG(query_string)
             
             WT_dict, LLRS = get_LLR(intersect_set, full, args.llr_csv)
